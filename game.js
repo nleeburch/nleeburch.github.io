e9a4d0ece623d85afdc1061ade0aac1ac96ea0ce
nleeburch <49600355+nleeburch@users.noreply.github.com>
2020-08-10 18:49:49 -0700

    Update game.js
    more progress on the game board

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 var player = {x: 1, y: 1, v: 1};
 var bullets = [
-{x: 3, y: 3, v: 1},
-{x: 11, y: 7, v: 1},
-{x: 4, y: 11, v: 1},
+{x: 4, y: 3, dx: 1, t: 0},
+{x: 10, y: 7, dx: -1, t: 0},
+{x: 5, y: 11, dx: 1, t: 0},
 ];
 
 let array = [
@@ -28,7 +28,11 @@ let array = [
 //4 = bullet
 //9 = player
 
-function DrawArray(arr, player) {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function DrawArray(arr, player) {
   document.body.innerHTML = "";
   for (let i = 0; i < 15; i++) {
     for (let j = 0; j < 15; j++) {
@@ -36,13 +40,13 @@ function DrawArray(arr, player) {
         document.write("<span style=\"font-family: Noto Mono;\">&#124;<\/span>");
       }
       else if (arr[i][j] === 0) { 
-        if((i == bullets[0].x) && (j == bullets[0].y)) {
+        if((j == bullets[0].x) && (i == bullets[0].y)) {
         document.write("<span style=\"font-family: Noto Mono;\">&#45;<\/span>");
         }
-        else if((i == bullets[1].x) && (j == bullets[1].y)) {
+        else if((j == bullets[1].x) && (i == bullets[1].y)) {
         document.write("<span style=\"font-family: Noto Mono;\">&#45;<\/span>");
         }
-        else if((i == bullets[2].x) && (j == bullets[2].y)) {
+        else if((j == bullets[2].x) && (i == bullets[2].y)) {
         document.write("<span style=\"font-family: Noto Mono;\">&#45;<\/span>");
         }
         else if ((i == player.x) && (j == player.y)) {
@@ -65,33 +69,47 @@ function DrawArray(arr, player) {
 
 async function MoveStar(arr, player, key) {
   //w 
-  if((key == 'w') && (arr[player.x - player.v][player.y] != 1)) {
+  if((key == 'w') && (arr[player.x - player.v][player.y] == 0)) {
     player.x -= player.v;
   }
   //a
-  else if ((key == 'a') && (arr[player.x][player.y - player.v] != 1)) {
+  else if ((key == 'a') && (arr[player.x][player.y - player.v] == 0)) {
     player.y -= player.v;
   }
   //s
-  else if ((key == 's') && (arr[player.x + player.v][player.y] != 1)) { 
+  else if ((key == 's') && (arr[player.x + player.v][player.y] == 0)) { 
     player.x += player.v;
   }
   //d
-  else if ((key == 'd') && (arr[player.x][player.y + player.v] != 1)) { 
+  else if ((key == 'd') && (arr[player.x][player.y + player.v] == 0)) { 
     player.y += player.v;
   }
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function MoveBullets(arr, bullets) {
+  for(let i = 0; i < 3; i++) {
+    if(arr[bullets[i].x + bullets[i].dx] !== 1) {
+      bullets[i].x += bullets[i].dx;
+      bullets[i].t++;
+      console.log("bullets[" + i + "].x = " + bullets[i].x);
+    }
+    else {
+      alert("ELSE");
+      let val = (bullets[i].dx * bullets[i].t)
+      bullets[i].x -= val;
+      bullets[i].t = 0;
+      console.log("bullets[" + i + "].t = " + bullets[i].t);
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////
 
 let interval = 1;
 
-async function GameLoop(){
+async function GameLoop() {
   DrawArray(array, player);
+  MoveBullets(array, bullets);
   document.addEventListener("keydown", 
     (event) => { 
       if(interval === 1){
@@ -100,8 +118,8 @@ async function GameLoop(){
       }
     });
   interval = 1;
-  await sleep(50);
+  await sleep(100);
   window.requestAnimationFrame(GameLoop);
 }
 
-GameLoop();
+window.requestAnimationFrame(GameLoop);
