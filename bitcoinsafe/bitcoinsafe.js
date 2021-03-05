c37edb508c53a31a64ed5db6ab5c6b9bdbc7aa8e
nleeburch <49600355+nleeburch@users.noreply.github.com>
2021-03-05 16:29:53 -0600

    big changes

--- bitcoinsafe/bitcoinsafe.js
+++ bitcoinsafe/bitcoinsafe.js
@@ -1,5 +1,5 @@
 let entries = [];
-let sortedEntries = [];
+let contributors = [];
 let rating = {
   1: "Very Low",
   2: "Low",
@@ -9,51 +9,32 @@ let rating = {
   instant: "Instant",
   free: "Free",
 };
+let highSecurity = [
+  "Yeti Bitcoin Wallet Level 3",
+  "Yeti Bitcoin Wallet Level 2",
+  "Yeti Bitcoin Wallet Level 1",
+  "Coinbase",
+];
+let fastSetup = [
+  "Trezor Standard Setup",
+  "Ledger Nano S Standard Setup",
+  "BlueWallet on Daily Phone",
+];
 
-/* 
-  it didn't upload correctly
-    KEY:
-    0 - name
-    1 - url
-    2 - completeinstructions
-    3 - comments (completeinstructions)
-    4 - setupspeed
-    5 - comments_2 (setupspeed)
-    6 - cost
-    7 - comments_3 (cost)
-    8 - privacy *
-    9 - comments_4 (privacy)
-    10 - theftprevention *
-    11 - comments_5 (theftprevention)
-    12 - lossprevention *
-    13 - comments_6 (lossprevention)
-    14 - safesetup *
-    15 - comments_7 (safesetup)
-    16 - inheritancesafety *
-    17 - comments_8 (inheritancesafety)
-    18 - spendspeed
-    19 - comments_9 (spendspeed)
-    20 - easytosetup
-
-    
-
-    <script src="bitcoinsafe.js"></script>
-    <script
-      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-      crossorigin="anonymous"
-    ></script>
-    */
+/*
+KEY:
+0 - name
+1 - url
+2 - instruction quality
+3 - privacy
+4 - theft prevention
+5 - loss prevention
+6 - inheritance safety
+7 - setup safety
+8 - spending speed
+9 - setup time
+10 - cost
+*/
 
 function eraseProducts() {
   for (let i = 1; i <= 4; i++) {
@@ -62,76 +43,43 @@ function eraseProducts() {
 }
 
 async function handleToggle() {
+  if (!document.getElementById("toggle").checked) {
+    document.getElementById("highsecurity").style.textDecoration = "underline";
+    document.getElementById("fastsetup").style.textDecoration = "none";
+  } else {
+    document.getElementById("highsecurity").style.textDecoration = "none";
+    document.getElementById("fastsetup").style.textDecoration = "underline";
+  }
   eraseProducts();
   fetchData();
 }
 
 function sortEntries() {
-  while (entries.length > 0) {
-    tempArray = [entries[0]];
-    for (let j = 1; j < entries.length; j++) {
-      //if we find a different review of the same name
-      if (entries[j][0] == entries[0][0]) {
-        //push that review onto tempArray
-        tempArray.push(entries[j]);
-        //then splice the review out of entries
-        entries.splice(j, 1);
-      }
-    }
-    //once you are all done, go ahead and splice out the first review
-    entries.splice(0, 1);
-    //create a mergedEntry
-    let mergedEntry = tempArray[0];
-    //calculate average ratings
-    let h = 1;
-    //addition of h values
-    for (h; h < tempArray.length; h++) {
-      for (let k = 2; k <= 18; k += 2) {
-        if (k !== 6) {
-          mergedEntry[k] =
-            parseFloat(mergedEntry[k]) + parseFloat(tempArray[h][k]);
-        }
-      }
-    }
-    //division by h
-    if (h > 1) {
-      for (let k = 2; k <= 18; k += 2) {
-        if (k !== 6) {
-          mergedEntry[k] /= h;
-        }
-      }
-    }
-    sortedEntries.push(mergedEntry);
-    i = 0;
-  }
-
   //filter sortedEntries for High Security
   if (!document.getElementById("toggle").checked) {
-    for (let j = 0; j < sortedEntries.length; j++) {
-      for (let i = j + 1; i < sortedEntries.length; i++) {
-        let x =
-          sortedEntries[j][10] + sortedEntries[j][12] + sortedEntries[j][16];
-        let y =
-          sortedEntries[i][10] + sortedEntries[i][12] + sortedEntries[i][16];
+    for (let j = 0; j < entries.length; j++) {
+      for (let i = j + 1; i < entries.length; i++) {
+        let x = entries[j][4] + entries[j][5] + entries[j][7];
+        let y = entries[i][4] + entries[i][5] + entries[i][7];
         if (y > x) {
-          let temp = sortedEntries[j];
-          sortedEntries[j] = sortedEntries[i];
-          sortedEntries[i] = temp;
+          let temp = entries[j];
+          entries[j] = entries[i];
+          entries[i] = temp;
           i = j + 1;
         }
       }
     }
   } else {
     //filter sortedEntries for Fast Setup
-    for (let j = 0; j < sortedEntries.length; j++) {
-      for (let i = j + 1; i < sortedEntries.length; i++) {
-        let x = parseFloat(sortedEntries[j][4]);
-        let y = parseFloat(sortedEntries[i][4]);
+    for (let j = 0; j < entries.length; j++) {
+      for (let i = j + 1; i < entries.length; i++) {
+        let x = parseFloat(entries[j][9]);
+        let y = parseFloat(entries[i][9]);
         if (y < x) {
           //replace [0] with [i] and vice versa
-          let temp = sortedEntries[j];
-          sortedEntries[j] = sortedEntries[i];
-          sortedEntries[i] = temp;
+          let temp = entries[j];
+          entries[j] = entries[i];
+          entries[i] = temp;
           i = j + 1;
         }
       }
@@ -139,34 +87,56 @@ function sortEntries() {
   }
 
   //reassign qualitative rating after sorting
-  for (let h = 0; h < sortedEntries.length; h++) {
-    for (let k = 8; k <= 16; k += 2) {
-      sortedEntries[h][k] = rating[Math.floor(sortedEntries[h][k])];
+  for (let h = 0; h < entries.length; h++) {
+    for (let k = 2; k <= 7; k++) {
+      entries[h][k] = rating[Math.floor(entries[h][k])];
     }
-    if (sortedEntries[h][4] == 0) {
-      sortedEntries[h][4] = "Instant";
+    if (entries[h][9] == 0) {
+      entries[h][9] = "Instant";
     } else {
-      sortedEntries[h][4] = parseFloat(sortedEntries[h][4]) + " hour(s)";
+      entries[h][9] = parseFloat(entries[h][9]) + " hour(s)";
     }
-    sortedEntries[h][18] = parseFloat(sortedEntries[h][18]) + " minute(s)";
+    entries[h][8] = parseFloat(entries[h][8]) + " minute(s)";
   }
-  return sortedEntries;
 }
 
 function buildElement(i, j, id, text, type, checked) {
-  let category = document.createElement(type);
-  category.id = id;
-  category.innerText = text + ": " + entries[i][j];
-  if (checked == true) {
+  if (id == "name") {
+    let category = document.createElement(type);
+    category.id = id;
+    category.innerText = entries[i][j];
+    category.style.fontSize = "18px";
+    category.classList.add("text-center");
     category.style.fontWeight = 800;
+    document.getElementById("product-" + (4 - i)).appendChild(category);
+  } else if (id == "url") {
+    let category = document.createElement(type);
+    category.id = id;
+    category.innerText = entries[i][j];
+    category.href = entries[i][j];
+    category.style.fontSize = "16px";
+    category.style.textDecoration = "underline";
+    if (4 - i == 3) {
+      category.style.color = "white";
+    } else {
+      category.style.color = "#181059";
+    }
+    document.getElementById("product-" + (4 - i)).appendChild(category);
+  } else {
+    let category = document.createElement(type);
+    category.id = id;
+    category.innerText = text + ": " + entries[i][j];
+    if (checked == true) {
+      category.style.fontWeight = 800;
+    }
+    document.getElementById("product-" + (4 - i)).appendChild(category);
   }
-  document.getElementById("product-" + (4 - i)).appendChild(category);
 }
 
 function showFour() {
   let checkFlag = !document.getElementById("toggle").checked;
   for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < sortedEntries.length; j++) {
+    for (let j = 0; j < entries[i].length; j++) {
       switch (j) {
         case 0: {
           buildElement(i, j, "name", "Name", "div");
@@ -180,25 +150,17 @@ function showFour() {
           buildElement(
             i,
             j,
-            "completeinstructions",
-            "Complete Instructions",
+            "instructionquality",
+            "Instruction Quality",
             "div"
           );
           break;
         }
-        case 4: {
-          buildElement(i, j, "setupspeed", "Setup Speed", "div", !checkFlag);
-          break;
-        }
-        case 6: {
-          buildElement(i, j, "cost", "Cost", "div");
-          break;
-        }
-        case 8: {
+        case 3: {
           buildElement(i, j, "privacy", "Privacy", "div");
           break;
         }
-        case 10: {
+        case 4: {
           buildElement(
             i,
             j,
@@ -209,7 +171,7 @@ function showFour() {
           );
           break;
         }
-        case 12: {
+        case 5: {
           buildElement(
             i,
             j,
@@ -220,11 +182,7 @@ function showFour() {
           );
           break;
         }
-        case 14: {
-          buildElement(i, j, "safesetup", "Safe Setup", "div");
-          break;
-        }
-        case 16: {
+        case 6: {
           buildElement(
             i,
             j,
@@ -235,8 +193,20 @@ function showFour() {
           );
           break;
         }
-        case 18: {
-          buildElement(i, j, "spendspeed", "Spend Speed");
+        case 7: {
+          buildElement(i, j, "setupsafety", "Setup Safety", "div", checkFlag);
+          break;
+        }
+        case 8: {
+          buildElement(i, j, "spendingspeed", "Spending Speed", "div");
+          break;
+        }
+        case 9: {
+          buildElement(i, j, "setuptime", "Setup Time", "div", !checkFlag);
+          break;
+        }
+        case 10: {
+          buildElement(i, j, "cost", "Cost", "div");
           break;
         }
       }
@@ -244,76 +214,94 @@ function showFour() {
   }
 }
 
-async function fetchData() {
-  entries = [];
-  sortedEntries = [];
-  /*
+async function fetchContributors() {
+  contributors = [];
   let response = await fetch(
-    "https://spreadsheets.google.com/feeds/list/1BxAiGnhCfFifd6T6Ky1m3gwdePvuS6QteXsc5KadlPI/od6/public/full?alt=json"
-  );
-  */
-  let response = await fetch(
-    "https://spreadsheets.google.com/feeds/list/1BxAiGnhCfFifd6T6Ky1m3gwdePvuS6QteXsc5KadlPI/od6/public/full?alt=json"
+    "https://spreadsheets.google.com/feeds/list/1BxAiGnhCfFifd6T6Ky1m3gwdePvuS6QteXsc5KadlPI/6/public/full?alt=json"
   );
   let data = await response.json();
   console.log(data);
   for (let i = 0; i < data.feed.entry.length; i++) {
     let newEntry = [
       data.feed.entry[i].gsx$name.$t,
-      data.feed.entry[i].gsx$url.$t,
-      data.feed.entry[i].gsx$completeinstructions.$t,
-      data.feed.entry[i].gsx$comments.$t,
-      data.feed.entry[i].gsx$setupspeed.$t,
-      data.feed.entry[i].gsx$comments_2.$t,
-      data.feed.entry[i].gsx$cost.$t,
-      data.feed.entry[i].gsx$comments_3.$t,
-      data.feed.entry[i].gsx$privacy.$t,
-      data.feed.entry[i].gsx$comments_4.$t,
-      data.feed.entry[i].gsx$theftprevention.$t,
-      data.feed.entry[i].gsx$comments_5.$t,
-      data.feed.entry[i].gsx$lossprevention.$t,
-      data.feed.entry[i].gsx$comments_6.$t,
-      data.feed.entry[i].gsx$safesetup.$t,
-      data.feed.entry[i].gsx$comments_7.$t,
-      data.feed.entry[i].gsx$inheritancesafety.$t,
-      data.feed.entry[i].gsx$comments_8.$t,
-      data.feed.entry[i].gsx$spendspeed.$t,
-      data.feed.entry[i].gsx$comments_9.$t,
-      data.feed.entry[i].gsx$easytosetupgreatux.$t,
+      data.feed.entry[i].gsx$twittername.$t,
+      data.feed.entry[i].gsx$description.$t,
     ];
+    contributors.push(newEntry);
+  }
+  for (let i = 0; i < contributors.length; i++) {
+    let profile = document.createElement("div");
+    profile.classList.add("col-2", "text-center", "profile");
+    let image = document.createElement("img");
+    image.src = "reviewers/JWWeatherman_.jpg";
+    image.classList.add("profile-picture");
+    let name = document.createElement("div");
+    name.innerText = contributors[i][0];
+    let twitterName = document.createElement("a");
+    twitterName.href = "https://twitter.com/" + contributors[i][1];
+    twitterName.innerText = "@" + contributors[i][1];
+    let description = document.createElement("div");
+    description.innerText = contributors[i][2];
+    profile.appendChild(image);
+    profile.appendChild(name);
+    profile.appendChild(twitterName);
+    profile.appendChild(description);
+    document.getElementById("contributors").appendChild(profile);
+  }
+}
 
-    for (let i = 0; i < newEntry.length; i++) {
-      switch (newEntry[i]) {
-        case rating[1]: {
-          newEntry[i] = 1;
-          break;
-        }
-        case rating[2]: {
-          newEntry[i] = 2;
-          break;
-        }
-        case rating[3]: {
-          newEntry[i] = 3;
-          break;
-        }
-        case rating[4]: {
-          newEntry[i] = 4;
-          break;
-        }
-        case rating[5]: {
-          newEntry[i] = 5;
-          break;
-        }
-        case rating.instant: {
-          newEntry[i] = 0;
-          break;
+async function fetchData() {
+  entries = [];
+  let response = await fetch(
+    "https://spreadsheets.google.com/feeds/list/1BxAiGnhCfFifd6T6Ky1m3gwdePvuS6QteXsc5KadlPI/2/public/full?alt=json"
+  );
+  let data = await response.json();
+  if (!document.getElementById("toggle").checked) {
+    for (let i = 0; i < data.feed.entry.length; i++) {
+      for (let j = 0; j < highSecurity.length; j++) {
+        if (data.feed.entry[i].gsx$name.$t == highSecurity[j]) {
+          let newEntry = [
+            data.feed.entry[i].gsx$name.$t,
+            data.feed.entry[i].gsx$url.$t,
+            data.feed.entry[i].gsx$instructionquality.$t,
+            data.feed.entry[i].gsx$privacy_2.$t,
+            data.feed.entry[i].gsx$theftprevention.$t,
+            data.feed.entry[i].gsx$lossprevention.$t,
+            data.feed.entry[i].gsx$inheritancesafety.$t,
+            data.feed.entry[i].gsx$setupsafety.$t,
+            data.feed.entry[i].gsx$spendingspeed.$t,
+            data.feed.entry[i].gsx$setuptime_3.$t,
+            data.feed.entry[i].gsx$cost.$t,
+          ];
+          entries.push(newEntry);
         }
       }
     }
-    entries.push(newEntry);
+  } else {
+    for (let i = 0; i < data.feed.entry.length; i++) {
+      for (let j = 0; j < fastSetup.length; j++) {
+        if (data.feed.entry[i].gsx$name.$t == fastSetup[j]) {
+          let newEntry = [
+            data.feed.entry[i].gsx$name.$t,
+            data.feed.entry[i].gsx$url.$t,
+            data.feed.entry[i].gsx$instructionquality.$t,
+            data.feed.entry[i].gsx$privacy_2.$t,
+            data.feed.entry[i].gsx$theftprevention.$t,
+            data.feed.entry[i].gsx$lossprevention.$t,
+            data.feed.entry[i].gsx$inheritancesafety.$t,
+            data.feed.entry[i].gsx$setupsafety.$t,
+            data.feed.entry[i].gsx$spendingspeed.$t,
+            data.feed.entry[i].gsx$setuptime_3.$t,
+            data.feed.entry[i].gsx$cost.$t,
+          ];
+          entries.push(newEntry);
+        }
+      }
+    }
   }
-  entries = sortEntries();
   showFour();
 }
 
+document.getElementById("highsecurity").style.textDecoration = "underline";
 fetchData();
+fetchContributors();
